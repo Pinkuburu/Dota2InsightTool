94fc51a6322553e961327a67a996bccb1605e77f
jack wade <jackwade19@gmail.com>
2020-10-06 22:13:39 +0100

    Initial commit

---
+++ Dota2APICall.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Dota2InsightTool
+{
+    class Dota2APICall
+    {
+        public static string APIResults;
+
+        public static async void GetData()
+        {
+
+            //We will make a GET request to a really cool website...
+
+            string baseUrl = "https://api.opendota.com/api/matches/5644760337";
+            //The 'using' will help to prevent memory leaks.
+            //Create a new instance of HttpClient
+            using (HttpClient client = new HttpClient())
+
+            //Setting up the response...         
+
+            using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+            using (HttpContent content = res.Content)
+            {
+                string data = await content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    Console.WriteLine(data);
+                    APIResults = data;
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dota2InsightTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Dota2APICall.GetData();
+            MessageBox.Show("Test");
+            //JToken token = JObject.Parse(Dota2APICall.APIResults);
+            MatchData myDeserializedClass = JsonConvert.DeserializeObject<MatchData>(Dota2APICall.APIResults);
+
+         
+            tMainTextBox.Text = Dota2APICall.APIResults;
+        }
+    }
+}
